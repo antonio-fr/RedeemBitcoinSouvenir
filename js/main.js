28e03267d2cbbe60ebdf5f9083f54cf3793670f0
a_ferron <anferron@gmail.com>
2018-01-15 15:29:48 +0100

    Add variable live fees
    confirmation box
    more outputs info

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ window.onload = function () {
 		document.body.scrollTop = document.body.scrollHeight;
 	};
 };
-function GetUtxo(btcAddress){
+function GetUtxo(){
 	console.log("Getting utxo");
 	$('#payaddr').hide();
 	$('#amnt').hide();
@@ -101,8 +101,7 @@ const BuildTransaction = function(txid, outid , destaddr, amount, script) {
 		"scriptPubKey": script,
 		"satoshis": 50000,
 	});
-	// Apprx 80 s/B pour la fee (output = 50000-80s/B*256B)
-	transaction.to(destaddr, 29500)
+	transaction.to(destaddr, outputtx)
 		.lockUntilDate(1514761200, 0);
 	var pvkeyuser = $('#pvkey').val();
 	var privateKey = bitcore.PrivateKey.fromWIF(pvkeyuser);
@@ -130,8 +129,7 @@ function end(message){
 function dotransaction(utxo_input, txid, outid){
 	$('#qrcode').hide();
 	console.log("Transaction generation and signing");
-	destaddr = $('#toaddr').val();
-	var transaction = BuildTransaction(txid, outid, destaddr, utxo_input.value, utxo_input.script)
+	var transaction = BuildTransaction(txid, outid, destaddrfunds, utxo_input.value, utxo_input.script)
 	$('.output').text("Transaction construite");
 	var pushtx = { tx: transaction.toString() };
 	$.post('https://api.blockcypher.com/v1/btc/main/txs/push', JSON.stringify(pushtx))
@@ -156,11 +154,59 @@ function redeem() {
 		.add( bitcore.crypto.BN.fromNumber( 1514761200 ).toScriptNumBuffer())
 		.add( 'OP_CHECKLOCKTIMEVERIFY' ).add('OP_DROP')
 		.add( bitcore.Script.buildPublicKeyHashOut( pubadr ));
-	var p2shAddress = bitcore.Address.payingTo( redeemScript );
+	p2shAddress = bitcore.Address.payingTo( redeemScript );
 	document.getElementById("loading").remove();
 	document.getElementById("gomove").style.display="none";
-	GetUtxo(p2shAddress);
+	getfee();
 };
+function toMB(nbsize){
+	return nbsize/1000000;
+}
+function FilterArr(FArray,limit){
+	var FilteredArray = FArray.filter(ArrEl=>ArrEl>limit);
+	return FilteredArray.length
+}
+function call(data){
+	var ranges = [  5,  10,  20,  30,  40,  50,  60,  70,  80,  90, 100, 120,
+				  140, 160, 180, 200, 220, 240, 260, 280, 300, 350, 400, 450,
+				  500, 550, 600, 650, 700, 750, 800, 850, 900, 950,1000];
+	var TxSizeArray = data[data.length-1][2].map(toMB);
+	var feeArrSlow = FilterArr(TxSizeArray,8); // Fast: 1, Norm: 4
+	feeSlow = ranges[feeArrSlow];
+	txfee = Math.ceil(80*256);
+	outputtx = 50000 - txfee;
+	if (outputtx < 10000){
+		$('.output').text("Fees réseau Bitcoin trop elevées");
+		alert("Désolé, les fees du réseau Bitcoin sont trop elevées pour vous permettre de retirer les fonds de votre Bitcoin Souvenir.");
+		return;
+	}
+	destaddrfunds = $('#toaddr').val();
+	var txtconf = "  >> CONFIRMATION <<";
+	txtconf += "\nDépense du Bitcoin Souvenir";
+	txtconf += "\n "+p2shAddress;
+	txtconf += "\n 0,0005 BTC";
+	txtconf += "\n\nTx vers "+destaddrfunds;
+	txtconf += "\n Montant : "+outputtx/1e8+" BTC";
+	txtconf += "\n Fees    : "+txfee/1e8+" BTC";
+	if (confirm(txtconf) == true) {
+		console.log("You pressed OK!");
+		GetUtxo();
+	} else {
+			$('.output').text("Opération annulé par l'utilisateur");
+	}
+}
+function getfee(){
+	$.ajax({
+		url: "https://core.jochen-hoenicke.de/queue/8h.js",
+		type: "GET",
+		cache: false,
+		dataType: "script"
+	}).fail(function (errorm,status) {
+			console.log(errorm);
+			console.log(status);
+			$('.output').text("ERREUR Internet");
+	});
+}
 function Move(){
 	if (document.getElementById('pvkey').value.length > 0 ){
 		$('#stopscan').hide();
@@ -169,6 +215,7 @@ function Move(){
 		var testfastWIF = privkey.startsWith("5");
 		if (!testfastWIF){alert("Entrez ou scannez vore CLEF PRIVEE du Bitcoin Souvenir"); return};
 		if (!/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/g.test($('#toaddr').val())){alert("Entrez une adresse de destination valide pour le dépenser"); return}; //
+		$('.output').text("PATIENTEZ ...");
 		setTimeout(redeem, 20);
 	}
 };
